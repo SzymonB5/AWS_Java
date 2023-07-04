e854c217b746cbb8d03e661836a0d0546240193a
SzymonB5 <235831@edu.p.lodz.pl>
2023-07-04 09:18:31 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql://localhost:5432/mydatabase";
+        String username = "postgres";
+        String password = "password";
+
+        try (Connection conn = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Connected to the PostgreSQL database!");
+
+            // Perform database operations here
+
+        } catch (SQLException e) {
+            System.out.println("Failed to connect to the database!");
+            e.printStackTrace();
+        }
+    }
+}
